18080f10ca9fd9def1d74869719b26bb02de9aad
Gabriel David <devgabrieldavid@gmail.com>
2025-04-25 16:25:04 -0300

    feito conexao com banco de dados zayamodas_db

---
+++ config-db.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'zayamodas_db';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+?>
