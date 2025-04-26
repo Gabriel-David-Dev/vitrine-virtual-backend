265c28a833ca64cccb82cbf88676f2e4656105b3
Gabriel David <devgabrieldavid@gmail.com>
2025-04-26 18:02:04 -0300

    add verificacao de login e redirecionamento em caso de sucesso ou exibicao de mensagem de erro para falha

--- verifica-login.php
+++ verifica-login.php
@@ -1,1 +1,22 @@
-<h1>ola mundo</h1>
+<?php
+
+    if(isset($_POST['entrar']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+
+        include_once('config-db.php');
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $sql  = "SELECT * FROM usuarios WHERE email_usuario = '$email' AND senha_usuario = '$senha'";
+        $result = $conexao->query($sql);
+
+        if(mysqli_num_rows($result) < 1) {
+            header('Location: ' . '../Frontend/login.php?erro=1');
+        }
+        else {
+            header('Location: ' . '../Frontend/index.html');
+        }
+    }
+    else {
+        header('Location: ' . '../Frontend/login.php');
+    }
+?>
