222c66771a12013a1d8ee7bd94a866589211f041
Gabriel David <devgabrieldavid@gmail.com>
2025-04-27 12:38:35 -0300

    add sessoes de login com php e limpar sessao em caso de falha

--- verifica-login.php
+++ verifica-login.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     if(isset($_POST['entrar']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
 
@@ -10,9 +11,14 @@
         $result = $conexao->query($sql);
 
         if(mysqli_num_rows($result) < 1) {
+            unset($_SESSION['email']);
+            unset($_SESSION['senha']);
             header('Location: ' . '../Frontend/login.php?erro=1');
         }
         else {
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] = $senha;
+            $_SESSION['logado'] = true;
             header('Location: ' . '../Frontend/index.html');
         }
     }
